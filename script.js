41a3ba478625e02f8d38220940340ee924f04bab
ysolis005-crypto <ysolis005@gmail.com>
2026-02-05 02:09:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function generar() {
   var nicho = document.getElementById("nicho").value;
   var resultado = document.getElementById("resultado");
-var btnWhatsapp = document.getElementById("btnWhatsapp");
-  
+  var btnWhatsapp = document.getElementById("btnWhatsapp");
   if (nicho === "") {
     resultado.innerHTML = "<p style='color:red;'>Por favor escribe un nicho</p>";
     return;
@@ -16,9 +15,7 @@ var btnWhatsapp = document.getElementById("btnWhatsapp");
     "<li>Mini curso en video de " + nicho + "</li>" +
     "<li>Plantillas listas para usar en " + nicho + "</li>" +
     "</ul>";
-  var mensaje = "Hola, quiero el plan completo del producto digital para el nicho: " + nicho;
-var mensajeCodificado = encodeURIComponent(mensaje);
-
-btnWhatsapp.href = "https://wa.me/573236651921?text=" + mensajeCodificado;
-
 }
+ var mensaje = "Hola, quiero el plan completo del producto digital para el nicho: " + nicho;
+var mensajeCodificado = encodeURIComponent(mensaje);
+btnWhatsapp.href = "https://wa.me/573236651921?text=" + mensajeCodificado;
